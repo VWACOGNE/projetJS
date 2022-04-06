e4ec158669c578dfa905b509a5c838a32aebc889
VWACOGNE <victor.wacogne@le-campus-numerique.fr>
2022-04-06 16:41:24 +0200

    Etape 4 - Fonctionnalité 1 - Réalisation du Feed (1 jour) #2 end of step 4

--- index.js
+++ index.js
@@ -1,48 +1,74 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $.ajax({
         //L'URL de la requête
-        url: "https://www.metaweather.com/api/location/44418/",
+
+        url: 'https://www.metaweather.com/api/location/609125',
 
         //La méthode d'envoi (type de requête)
         method: "GET",
 
         //Le format de réponse attendu
-        dataType : "json",
+        dataType: "json",
     })
-        //Ce code sera exécuté en cas de succès - La réponse du serveur est passée à done()
-        /*On peut par exemple convertir cette réponse en chaine JSON et insérer
-         * cette chaine dans un div id="res"*/
-        .done(function(response){
-            for (let id = 0; id < response.consolidated_weather.length; id++){
-                console.log(response.title)
-            console.log(response.consolidated_weather[id])
-                console.log(response.consolidated_weather[id].applicable_date)
-                console.log(response.consolidated_weather[id].weather_state_name)
-                let article = $('<article>');
-                let title = $('<h4>');
-                let date = $('<p>');
-                let temp= $('<p>');
-
-                title.text(response.title);
-                date.text(response.consolidated_weather[id].applicable_date);
-                temp.text(response.consolidated_weather[id].weather_state_name)
-                article.append(title, date, temp);
-                $('.feed').prepend(article);
-          //  let data = JSON.stringify(response);
-          //  $("div#res").append(data);
-            }
-
-            //$('<p>').html(response.consolidated_weather[id].applicable_date).appendTo('article');
+        .done(function (response) {
+            createArticle(response);
         })
-
-        //Ce code sera exécuté en cas d'échec - L'erreur est passée à fail()
-        //On peut afficher les informations relatives à la requête et à l'erreur
-        .fail(function(error){
-            alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
-        })
-
-        //Ce code sera exécuté que la requête soit un succès ou un échec
-        .always(function(){
-            alert("Requête effectuée");
-        });
 });
+
+function printMeteo(input) {
+    $.ajax({
+        url: "https://www.metaweather.com/api/location/search/?query=" + input,
+        method: "GET",
+        dataType: "json",
+    })
+        .done(function (response) {
+            let infoVille = response[0].woeid;
+            $.ajax({
+                //L'URL de la requête
+
+                url: 'https://www.metaweather.com/api/location/' + infoVille,
+
+                //La méthode d'envoi (type de requête)
+                method: "GET",
+
+                //Le format de réponse attendu
+                dataType: "json",
+            })
+                .done(function (response) {
+                    createArticle(response);
+                })
+        })
+        .fail(function (error) {
+            let p = $('<p class="messageError">');
+            p.text(("La requête s'est terminée en échec. Infos : " + JSON.stringify(error)));
+            $('.formulaire').append(p);
+        })
+}
+
+//fonction qui créé mes articles
+function createArticle(resultR) {
+    $('.feed').empty()
+    for (let id = 0; id < resultR.consolidated_weather.length; id++) {
+        let article = $('<article>');
+        let title = $('<h4>');
+        let date = $('<p>');
+        let temp = $('<p>');
+        let image = $('<img>');
+        let temperature = $('<p>');
+
+        title.text(resultR.title);
+        date.text(resultR.consolidated_weather[id].applicable_date);
+        temp.text(resultR.consolidated_weather[id].weather_state_name);
+        let info = resultR.consolidated_weather[id].weather_state_abbr;
+        image.attr("src", 'https://www.metaweather.com/static/img/weather/png/64/' + info + '.png');
+        temperature.text('Température max:'+resultR.consolidated_weather[id].max_temp+'°');
+
+        article.append(title, date, temp, image, temperature);
+        $('.feed').append(article);
+    }
+}
+
+function getValue() {
+    let input = document.getElementById("ville").value;
+    printMeteo(input);
+}
